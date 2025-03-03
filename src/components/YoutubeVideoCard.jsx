35e06d332eac8d50f3bfba541d26aa5f7b5ddc6c
Angel Moreno Flores <morenofl@usc.edu>
2025-03-02 23:14:56 -0800

    Initial commit for youtube-video

---
+++ src/components/YoutubeVideoCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import COLORS from '../constants/colors';
+import BookmarkIcon from '../assets/icons/bookmark.svg'; // Ensure you have an SVG or replace with an Image
+
+
+export default function YoutubeVideoCard() {
+
+	return (
+		<View>
+			<Text>
+				
+			</Text>
+
+
+		</View>
+
+	);
+
+}
+
+
--- src/screens/VideoScreen.tsx
+++ src/screens/VideoScreen.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import COLORS from '../constants/colors';
+import YoutubeVideoCard from '../components/YoutubeVideoCard';
 
 const VideoScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content} />
+            <YoutubeVideoCard/>
         </SafeAreaView>
     );
 };
